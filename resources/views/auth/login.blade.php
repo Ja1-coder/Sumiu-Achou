1bb03cd8e4352f359b0b549d1a13c6f3a9036e4e
Ja1-coder <jvcproenca@gmail.com>
2025-11-18 20:53:59 -0300

    feat: importando icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,19 +38,6 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Lembre-me e Esqueci minha senha -->
-        <div class="block flex flex-row gap-4 mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-[#F4F4F2]">{{ __('Lembre-me') }}</span>
-            </label>
-
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-[#F4F4F2] hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Esqueceu sua senha?') }}
-                </a>
-            @endif
-        </div>
 
         <div class="flex flex-col items-center justify-end mt-4">
             <x-primary-button class="ms-3">
